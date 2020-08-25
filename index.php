c778260255a6b7ced005074f8e0abc162e2b16b0
TOLBAR <tolbarweb@gmail.com>
2020-08-24 23:29:26 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,322 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Eventos DVM</title>
+  <link rel="icon" href="imagenes/logo.png">
+
+  <link rel="stylesheet" type="text/css" href="css/semantic.min.css">
+  <link rel="stylesheet" type="text/css" href="css/mi-estilo.css">
+</head>
+
+<body>
+<!-- Menu fijo cuando baja la pagina -->
+<div class="ui large top fixed hidden menu">
+  <div class="ui container">
+    <a class="item"> 
+        <img src="imagenes/logo-grande.png" style="width:80px">
+    </a>
+    <a class="active item" href="#inicio">Inicio</a>
+    <a class="item" href="#eventos">Eventos</a>
+    <a class="item" href="#quienes-somos">Quienes somos</a>
+    <a class="item" href="#contactenos">Contactenos</a>
+    <div class="right menu">
+      <div class="item">
+        <a class="ui button" href="login.php">Iniciar sesion</a>
+      </div>
+      <div class="item">
+        <a class="ui primary button" href="login.php">Registrarse</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Contenido -->
+<div class="pusher">
+  <!-- Menu Principal -->
+  <div class="ui inverted vertical masthead center aligned segment">
+    <div class="ui container">
+      <div class="ui large secondary inverted pointing menu">
+        <a class="item"> 
+            <img src="imagenes/logo-grande.jpeg" style="width:80px">
+        </a>
+        <a class="active item" href="#inicio">Inicio</a>
+        <a class="item" href="#eventos">Eventos</a>
+        <a class="item" href="#quienes-somos">Quienes somos</a>
+        <a class="item" href="#contactenos">Contactenos</a>
+        <div class="right item">
+          <a class="ui inverted button" href="login.php">Iniciar sesion</a>
+          <a class="ui inverted button" href="login.php">Registrarse</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="ui text container">
+      <h1 class="ui inverted header">
+        Eventos DVM
+      </h1>
+      <h2>Promociona tu evento con los mejores...</h2>      
+    </div>
+  </div>
+
+  <div class="ui vertical stripe segment" id="inicio">
+     <div class="ui middle aligned stackable grid container">
+      <div class="row">
+        
+        <div class="eight wide column">
+          <h3 class="ui header">Conocemos del evento que deseas</h3>
+          <p>
+            A través del tiempo se ha evidenciado la necesidad de celebrar con familiares y amigos alguna fecha especial; cumpleaños, bautizos, bodas, conciertos entre otros. Antes se utilizaban medios que se demoraban más tiempo y dinero para llevar la información de un evento al público; en la actualidad, el deseo de celebrar con las personas persiste pero la poca disponibilidad de tiempo y dinero para entregar la información del evento hace que genere en el anfitrión un nivel de estrés que no le permita disfrutar del evento y en muchos casos ni la fiesta misma. 
+          </p>
+          <h3 class="ui header">Por que nos interesas</h3>
+          <p>
+            Ante la carencia en el mercado, de empresas de eventos que ofrezcan servicios de calidad e innovadores, surge la idea de un aplicativo web que se dedique a promocionar eventos en la ciudad de Neiva.
+          </p>
+        </div>
+        <div class="six wide right floated column">
+          <img src="imagenes/foto-evento.jpg" class="ui large bordered rounded image">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="ui vertical stripe segment" id="eventos">
+     <div class="ui middle aligned stackable grid container">
+      <div class="row">
+      <div class="ui link cards">
+  <div class="card">
+    <div class="image">
+      <img src="imagenes/otro-evento.jpg">
+    </div>
+    <div class="content">
+      <div class="header">Matt Giampietro</div>
+      <div class="meta">
+        <a>Friends</a>
+      </div>
+      <div class="description">
+        Matthew is an interior designer living in New York.
+      </div>
+    </div>
+    <div class="extra content">
+      <span class="right floated">
+        Joined in 2013
+      </span>
+      <span>
+        <i class="user icon"></i>
+        75 Friends
+      </span>
+    </div>
+  </div>
+  <div class="card">
+    <div class="image">
+      <img src="imagenes/otro-evento.jpg">
+    </div>
+    <div class="content">
+      <div class="header">Molly</div>
+      <div class="meta">
+        <span class="date">Coworker</span>
+      </div>
+      <div class="description">
+        Molly is a personal assistant living in Paris.
+      </div>
+    </div>
+    <div class="extra content">
+      <span class="right floated">
+        Joined in 2011
+      </span>
+      <span>
+        <i class="user icon"></i>
+        35 Friends
+      </span>
+    </div>
+  </div>
+  <div class="card">
+    <div class="image">
+      <img src="imagenes/mesa-evento.jpg">
+    </div>
+    <div class="content">
+      <div class="header">Elyse</div>
+      <div class="meta">
+        <a>Coworker</a>
+      </div>
+      <div class="description">
+        Elyse is a copywriter working in New York.
+      </div>
+    </div>
+    <div class="extra content">
+      <span class="right floated">
+        Joined in 2014
+      </span>
+      <span>
+        <i class="user icon"></i>
+        151 Friends
+      </span>
+    </div>
+  </div>
+  <div class="card">
+    <div class="image">
+      <img src="imagenes/mesa-evento.jpg">
+    </div>
+    <div class="content">
+      <div class="header">Elyse</div>
+      <div class="meta">
+        <a>Coworker</a>
+      </div>
+      <div class="description">
+        Elyse is a copywriter working in New York.
+      </div>
+    </div>
+    <div class="extra content">
+      <span class="right floated">
+        Joined in 2014
+      </span>
+      <span>
+        <i class="user icon"></i>
+        151 Friends
+      </span>
+    </div>
+  </div>
+  <div class="card">
+    <div class="image">
+      <img src="imagenes/mesa-evento.jpg">
+    </div>
+    <div class="content">
+      <div class="header">Elyse</div>
+      <div class="meta">
+        <a>Coworker</a>
+      </div>
+      <div class="description">
+        Elyse is a copywriter working in New York.
+      </div>
+    </div>
+    <div class="extra content">
+      <span class="right floated">
+        Joined in 2014
+      </span>
+      <span>
+        <i class="user icon"></i>
+        151 Friends
+      </span>
+    </div>
+  </div>
+  <div class="card">
+    <div class="image">
+      <img src="imagenes/mesa-evento.jpg">
+    </div>
+    <div class="content">
+      <div class="header">Elyse</div>
+      <div class="meta">
+        <a>Coworker</a>
+      </div>
+      <div class="description">
+        Elyse is a copywriter working in New York.
+      </div>
+    </div>
+    <div class="extra content">
+      <span class="right floated">
+        Joined in 2014
+      </span>
+      <span>
+        <i class="user icon"></i>
+        151 Friends
+      </span>
+    </div>
+  </div>
+</div>
+
+      </div>
+    </div>
+  </div>
+
+  <div class="ui vertical stripe segment" id="quienes-somos">
+     <div class="ui middle aligned stackable grid container">
+      <div class="row">
+        
+        <div class="eight wide column">
+          <h3 class="ui header">Conocemos del evento que deseas</h3>
+          <p>
+            A través del tiempo se ha evidenciado la necesidad de celebrar con familiares y amigos alguna fecha especial; cumpleaños, bautizos, bodas, conciertos entre otros. Antes se utilizaban medios que se demoraban más tiempo y dinero para llevar la información de un evento al público; en la actualidad, el deseo de celebrar con las personas persiste pero la poca disponibilidad de tiempo y dinero para entregar la información del evento hace que genere en el anfitrión un nivel de estrés que no le permita disfrutar del evento y en muchos casos ni la fiesta misma. 
+          </p>
+          <h3 class="ui header">Por que nos interesas</h3>
+          <p>
+            Ante la carencia en el mercado, de empresas de eventos que ofrezcan servicios de calidad e innovadores, surge la idea de un aplicativo web que se dedique a promocionar eventos en la ciudad de Neiva.
+          </p>
+        </div>
+        <div class="six wide right floated column">
+          <img src="imagenes/foto-evento.jpg" class="ui large bordered rounded image">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="ui vertical stripe segment" id="contactenos">
+     <div class="ui middle aligned stackable grid container">
+      <div class="row">
+        
+        <div class="eight wide column">
+          <h3 class="ui header">Conocemos del evento que deseas</h3>
+          <p>
+            A través del tiempo se ha evidenciado la necesidad de celebrar con familiares y amigos alguna fecha especial; cumpleaños, bautizos, bodas, conciertos entre otros. Antes se utilizaban medios que se demoraban más tiempo y dinero para llevar la información de un evento al público; en la actualidad, el deseo de celebrar con las personas persiste pero la poca disponibilidad de tiempo y dinero para entregar la información del evento hace que genere en el anfitrión un nivel de estrés que no le permita disfrutar del evento y en muchos casos ni la fiesta misma. 
+          </p>
+          <h3 class="ui header">Por que nos interesas</h3>
+          <p>
+            Ante la carencia en el mercado, de empresas de eventos que ofrezcan servicios de calidad e innovadores, surge la idea de un aplicativo web que se dedique a promocionar eventos en la ciudad de Neiva.
+          </p>
+        </div>
+        <div class="six wide right floated column">
+          <img src="imagenes/foto-evento.jpg" class="ui large bordered rounded image">
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+  <div class="ui inverted vertical footer segment">
+    <div class="ui container">
+      <div class="ui stackable inverted divided equal height stackable grid">
+
+        <div class="seven wide column">
+          <h4 class="ui inverted header">Realizado por</h4>
+          <ul>
+            <li>dagna nikol duarte</li>
+            <li>vivian camila tamayo</li>
+            <li>loren michell tovar moreno</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<script src="js/jquery-3.1.1.min.js"></script>
+<script src="js/semantic.min.js"></script>
+<script>
+  $(document)
+    .ready(function() {
+
+      // fix menu when passed
+      $('.masthead')
+        .visibility({
+          once: false,
+          onBottomPassed: function() {
+            $('.fixed.menu').transition('fade in');
+          },
+          onBottomPassedReverse: function() {
+            $('.fixed.menu').transition('fade out');
+          }
+        })
+      ;
+
+      // create sidebar and attach to menu open
+      $('.ui.sidebar')
+        .sidebar('attach events', '.toc.item')
+      ;
+
+    })
+  ;
+  </script>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Eventos DVM</title>
+  <link rel="icon" href="imagenes/logo.png">
+
+  <link rel="stylesheet" type="text/css" href="css/semantic.min.css">
+  <link rel="stylesheet" type="text/css" href="css/mi-estilo.css">
+</head>
+
+<body>
+  <div class="ui placeholder segment">
+    <div class="ui two column very relaxed stackable grid">
+      <div class="column">
+        <div class="ui form">
+          <div class="field">
+            <label>Usuario</label>
+            <div class="ui left icon input">
+              <input type="text" placeholder="Username">
+              <i class="user icon"></i>
+            </div>
+          </div>
+          <div class="field">
+            <label>Contraseña</label>
+            <div class="ui left icon input">
+              <input type="password">
+              <i class="lock icon"></i>
+            </div>
+          </div>
+          <div class="ui blue submit button">Ingresar</div>
+        </div>
+      </div>
+      <div class="middle aligned column">
+        <div class="ui big button">
+          <i class="signup icon"></i>
+          Registrarse
+        </div>
+      </div>
+    </div>
+    <div class="ui vertical divider">
+      O
+    </div>
+  </div>
+
+<script src="js/jquery-3.1.1.min.js"></script>
+<script src="js/semantic.min.js"></script>
+</body>
+</html>
